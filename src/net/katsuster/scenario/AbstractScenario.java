11a74f89fadf503d73107b2980145b4702cc82e7
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2024-02-18 03:03:10 +0900

    scenario: enable to draw Drawable objects

--- src/net/katsuster/scenario/AbstractScenario.java
+++ src/net/katsuster/scenario/AbstractScenario.java
@@ -1,10 +1,19 @@
 package net.katsuster.scenario;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.katsuster.draw.Drawable;
 
 public class AbstractScenario implements Scenario {
     private ScenarioSwitcher switcher;
     private boolean activated = false;
+    private List<Drawable> listDrawable = new ArrayList<>();
+
+    public AbstractScenario(ScenarioSwitcher sw) {
+        switcher = sw;
+    }
 
     @Override
     public String getName() {
@@ -43,5 +52,31 @@ public class AbstractScenario implements Scenario {
     @Override
     public void drawFrame(Graphics2D g2) {
         //do nothing
+    }
+
+    public void clearDrawable() {
+        listDrawable.clear();
+    }
+
+    public void addDrawable(Drawable d) {
+        listDrawable.add(d);
+    }
+
+    public Drawable[] getDrawables() {
+        return (Drawable[])listDrawable.toArray();
+    }
+
+    public void removeDrawable(Drawable d) {
+        listDrawable.remove(d);
+    }
+
+    public void drawAllDrawable(Graphics2D g2) {
+        for (Drawable d : listDrawable) {
+            if (!d.getVisible()) {
+                continue;
+            }
+
+            d.draw(g2);
+        }
     }
 }
