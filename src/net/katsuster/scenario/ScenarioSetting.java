b0a8136d48925c8039483f300d9a43eec9995fea
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2024-02-17 22:07:23 +0900

    scenario: enable to load/save setting from user preferences

--- src/net/katsuster/scenario/ScenarioSetting.java
+++ src/net/katsuster/scenario/ScenarioSetting.java
@@ -1,9 +1,14 @@
 package net.katsuster.scenario;
 
 import java.awt.*;
+import java.util.prefs.Preferences;
+
+import net.katsuster.ui.MainWindow;
 
 public class ScenarioSetting {
-    private Font font;
+    public static String SETTING_HOST_NAME = "HostName";
+
+    private Font font = new Font(Font.MONOSPACED, Font.PLAIN, 16);
 
     public ScenarioSetting() {
         //do nothing
@@ -15,5 +20,17 @@ public class ScenarioSetting {
 
     public void setFont(Font f) {
         font = f;
+    }
+
+    public static String getSetting(String key) {
+        Preferences prefs = Preferences.userNodeForPackage(MainWindow.class);
+
+        return prefs.get(key, "");
+    }
+
+    public static void putSetting(String key, String value) {
+        Preferences prefs = Preferences.userNodeForPackage(MainWindow.class);
+
+        prefs.put(key, value);
     }
 }
