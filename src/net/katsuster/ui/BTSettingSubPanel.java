60cc46f1c00ee3d4187101dd267d16a2260b62cb
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2024-02-20 00:19:38 +0900

    ui: fix default UUID values of TX/RX

--- src/net/katsuster/ui/BTSettingSubPanel.java
+++ src/net/katsuster/ui/BTSettingSubPanel.java
@@ -11,8 +11,8 @@ import net.katsuster.ble.BTSetting;
 
 public class BTSettingSubPanel extends JPanel {
     public static final String DEFAULT_UUID_SERVICE = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
-    public static final String DEFAULT_UUID_RX = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
-    public static final String DEFAULT_UUID_TX = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
+    public static final String DEFAULT_UUID_TX = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+    public static final String DEFAULT_UUID_RX = "6e400003-b5a3-f393-e0a9-e50e24dcca9e";
 
     public static final String ACT_SET_DEFAULT = "Default";
     public static final String ACT_LOAD_SETTING = "Load";
