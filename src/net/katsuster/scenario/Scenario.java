78a36059c587c4b60f3a65711e64221b6209c086
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2024-03-06 04:29:32 +0900

    scenario: change font size to more small

--- src/net/katsuster/scenario/Scenario.java
+++ src/net/katsuster/scenario/Scenario.java
@@ -8,8 +8,8 @@ public interface Scenario {
     public static final Color COLOR_DARK_BLUE = new Color(40, 85, 160);
     public static final Color COLOR_DARK_ORANGE = new Color(247, 119, 15);
 
-    public static final int FONT_SIZE_LARGEST = 120;
-    public static final int FONT_SIZE_LARGE = 80;
+    public static final int FONT_SIZE_LARGEST = 96;
+    public static final int FONT_SIZE_LARGE = 64;
     public static final int FONT_SIZE_MEDIUM = 32;
     public static final int FONT_SIZE_SMALL = 24;
     public static final int FONT_SIZE_SMALLEST = 16;
