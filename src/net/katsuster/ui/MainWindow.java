d7c56043e1c1c0076969860dc3e6ae23d74ac7d0
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2024-03-06 04:56:24 +0900

    ui: enable to hold backbuffer in MainWindow

--- src/net/katsuster/ui/MainWindow.java
+++ src/net/katsuster/ui/MainWindow.java
@@ -1,12 +1,32 @@
 package net.katsuster.ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.VolatileImage;
 
 public class MainWindow extends JFrame {
+    private Image img;
+
     public MainWindow() {
         setTitle("Main");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //setUndecorated(true);
         setSize(1024, 768);
+    }
+
+    public void setImg(VolatileImage i) {
+        img = i;
+    }
+
+    @Override
+    public void update(Graphics g) {
+        paint(g);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        if (img != null) {
+            g.drawImage(img, 0, 0, this);
+        }
     }
 }
