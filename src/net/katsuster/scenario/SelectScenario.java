94159cae1e48d59389f33473233a131ff9ed3a67
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2024-03-30 04:38:04 +0900

    scenario: enable to select game scenario

--- src/net/katsuster/scenario/SelectScenario.java
+++ src/net/katsuster/scenario/SelectScenario.java
@@ -2,30 +2,39 @@ package net.katsuster.scenario;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
-import java.awt.geom.RoundRectangle2D;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.StringTokenizer;
 
 import net.katsuster.ble.BTInOut;
 import net.katsuster.draw.Drawable;
 import net.katsuster.draw.GridBG;
-import net.katsuster.draw.ShapeBox;
 import net.katsuster.draw.TextLine;
 import net.katsuster.ui.MainWindow;
 import net.katsuster.ui.MouseAdapterEx;
 
 public class SelectScenario extends AbstractScenario {
+    public static final String SCENARIO_COUNT_UP = "Count Up";
+    public static final String SCENARIO_TIME_ATTACK = "Time Attack";
+
     private BTDeviceHandler handlerBT;
     private MouseHandler handlerMouse;
     private Font fontLarge;
     private Font fontSmall;
     private boolean flagStart = false;
-    private TextLine tlMsg;
+    private boolean flagClose = false;
+    private TextLine tlSelector;
     private TextLine tlClock;
+    private List<String> scenarios = new ArrayList<>();
+    private int indexSelected = 0;
 
     public SelectScenario(ScenarioSwitcher sw) {
         super(sw);
+
+        scenarios.add(SCENARIO_COUNT_UP);
+        scenarios.add(SCENARIO_TIME_ATTACK);
     }
 
     @Override
@@ -58,13 +67,27 @@ public class SelectScenario extends AbstractScenario {
         tlTitle.getContentBox().setBounds(0, 0,
                 mainWnd.getWidth(), mainWnd.getHeight() / 2);
 
-        tlMsg = new TextLine();
-        tlMsg.setText("Select the game");
-        tlMsg.setAlign(Drawable.H_ALIGN.CENTER, Drawable.V_ALIGN.CENTER);
-        tlMsg.setForeground(Color.DARK_GRAY);
-        tlMsg.setFont(fontLarge);
-        tlMsg.getContentBox().setBounds(0, 0,
-                mainWnd.getWidth(), mainWnd.getHeight());
+        tlSelector = new TextLine();
+        tlSelector.setText(">>");
+        tlSelector.setAlign(Drawable.H_ALIGN.RIGHT, Drawable.V_ALIGN.TOP);
+        tlSelector.setForeground(COLOR_DARK_ORANGE);
+        tlSelector.setFont(fontLarge);
+        tlSelector.getContentBox().setBounds(0, mainWnd.getHeight() / 2,
+                mainWnd.getWidth() / 4 - FONT_SIZE_LARGE / 2, (int)(FONT_SIZE_LARGE * 1.3));
+
+        List<TextLine> tlScenarios = new ArrayList<>();
+        for (int i = 0; i < scenarios.size(); i++) {
+            TextLine tl = new TextLine();
+            tl.setText(scenarios.get(i));
+            tl.setAlign(Drawable.H_ALIGN.LEFT, Drawable.V_ALIGN.TOP);
+            tl.setForeground(Color.DARK_GRAY);
+            tl.setFont(fontLarge);
+            tl.getContentBox().setBounds(mainWnd.getWidth() / 4,
+                    mainWnd.getHeight() / 2 + (int)(FONT_SIZE_LARGE * 1.3) * i,
+                    mainWnd.getWidth() / 4 * 2, (int)(FONT_SIZE_LARGE * 1.3));
+
+            tlScenarios.add(tl);
+        }
 
         tlClock = new TextLine();
         tlClock.setAlign(Drawable.H_ALIGN.LEFT, Drawable.V_ALIGN.BOTTOM);
@@ -86,9 +109,12 @@ public class SelectScenario extends AbstractScenario {
         clearDrawable();
         addDrawable(bg);
         addDrawable(tlTitle);
+        addDrawable(tlSelector);
         addDrawable(tlClock);
         addDrawable(tlVersion);
-        addDrawable(tlMsg);
+        for (TextLine tl: tlScenarios) {
+            addDrawable(tl);
+        }
     }
 
     @Override
@@ -102,18 +128,57 @@ public class SelectScenario extends AbstractScenario {
 
     @Override
     public void drawFrame(Graphics2D g2) {
+        MainWindow mainWnd = getSwitcher().getMainWindow();
+
+        tlSelector.getContentBox().setBounds(0,
+                mainWnd.getHeight() / 2 + (int)(FONT_SIZE_LARGE * 1.3) * indexSelected,
+                mainWnd.getWidth() / 4 - FONT_SIZE_LARGE / 2, (int)(FONT_SIZE_LARGE * 1.3));
+
         SimpleDateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         tlClock.setText(df.format(new Date()));
 
         if (flagStart) {
-            getSwitcher().setNextScenario(new SingleScenario(getSwitcher()));
+            Scenario s = getSelectedScenario();
+            if (s == null) {
+                printError("Cannot select scenario, go to title.", null);
+                s = new SelectScenario(getSwitcher());
+            }
+
+            getSwitcher().setNextScenario(s);
+        }
+        if (flagClose) {
+            getSwitcher().setNextScenario(new ClosingScenario(getSwitcher()));
         }
 
         drawAllDrawable(g2);
     }
 
-    public synchronized void setFlagStart(boolean f) {
-        flagStart = f;
+    private Scenario getSelectedScenario() {
+        switch (scenarios.get(indexSelected)) {
+        case SCENARIO_TIME_ATTACK:
+            return new SingleScenario(getSwitcher());
+        case SCENARIO_COUNT_UP:
+        default:
+            printError("Invalid scenario is selected.", null);
+            break;
+        }
+
+        return null;
+    }
+
+    public synchronized void nextSelection() {
+        indexSelected++;
+        if (indexSelected >= scenarios.size()) {
+            indexSelected = 0;
+        }
+    }
+
+    public synchronized void startScenario() {
+        flagStart = true;
+    }
+
+    public synchronized void closeScenario() {
+        flagClose = true;
     }
 
     protected class BTDeviceHandler extends BTCommandHandler {
@@ -153,17 +218,17 @@ public class SelectScenario extends AbstractScenario {
 
         @Override
         public void mouseLeftClicked() {
-
+            scenario.nextSelection();
         }
 
         @Override
         public void mouseRightClicked() {
-            //scenario.closeScenario();
+            scenario.closeScenario();
         }
 
         @Override
         public void mouseLeftLongPressed() {
-            scenario.setFlagStart(true);
+            scenario.startScenario();
         }
     }
 }
