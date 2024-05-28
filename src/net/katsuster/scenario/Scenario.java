afd1c203d86dcdcf15de5ed35abfc50bdd4ad8a9
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2024-05-28 23:14:24 +0900

    v1.0-rc2

--- src/net/katsuster/scenario/Scenario.java
+++ src/net/katsuster/scenario/Scenario.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public interface Scenario {
     String CODE_NAME = "target-6-client";
-    String CODE_VERSION = "v1.0-rc1";
+    String CODE_VERSION = "v1.0-rc2";
     String CODE_TITLE_WORD1 = "Speed Shooting";
     String CODE_TITLE_WORD2 = "Scoreboard";
 
