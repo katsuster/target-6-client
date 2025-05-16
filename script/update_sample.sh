b02a1babbc55d04dee3ce2483da4ede8caa3d266
Katsuhiro Suzuki <katsuhiro@katsuster.net>
2025-05-17 00:35:11 +0900

    script: check update media path

--- script/update_sample.sh
+++ script/update_sample.sh
@@ -7,14 +7,31 @@
 #   - Mount storage on /mnt/sda1
 # - Run this script
 
-set -ex
+set -e
+
+# Default values
+
+if [ "x${MEDIA_PATH}" = "x" ]; then
+	MEDIA_PATH=/mnt/sda1
+fi
+REPO_NEW_PATH=${MEDIA_PATH}/rock/target-6-client
+
+# Check path
+if [ ! -d ${REPO_NEW_PATH}/.git ]; then
+	echo "Please mount update media on ${MEDIA_PATH}"
+	exit 1
+fi
+
+set -x
 
 cd ~/target-6-client
-git remote add update /mnt/sda1/rock/target-6-client
+if git remote show | grep update; then
+	git remote remove update
+fi
+git remote add update ${REPO_NEW_PATH}
 git fetch --prune --tags update
 git rebase update/main
 git remote remove update
 
-rm -r target
+rm -rf target
 mvn package
-
